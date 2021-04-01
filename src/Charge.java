9c8db8e663011c87729663d53ac2d659a0b5d326
union93 <union93@konkuk.ac.kr>
2021-04-01 23:34:29 +0900

    Initial commit

---
+++ src/Charge.java
@@ -0,0 +1,56 @@
+public class Charge {
+
+    public static final int BUS = 0;
+    public static final int TAXI = 1;
+    public static final int SUBWAY = 2;
+
+    public double calculate(int type, int age, int killometer) {
+        double result = 0;
+        switch(type) {
+            case (BUS) :
+                result = calculateBus(age, killometer);
+                break;
+            case (TAXI):
+                result = calculateTaxi(age, killometer);
+                break;
+            case (SUBWAY):
+                result = calculateSubway(age, killometer);
+                break;
+            default :
+                throw new RuntimeException("Such type does not exist");
+        }
+        return result;
+    }
+
+    public double calculateBus(int age, int kilometer) {
+        double baseBusCharge = 600;
+        if(age < 15 ) {
+            baseBusCharge = baseBusCharge * 0.5;
+        }else if(age > 60) {
+            baseBusCharge = baseBusCharge * 0.7;
+        }
+        return baseBusCharge;
+    }
+
+    public double calculateTaxi(int age, int kilometer) {
+        double baseTaxiCharge = 3000;
+        return baseTaxiCharge * kilometer;
+    }
+
+    public double calculateSubway(int age, int kilometer) {
+        double baseSubwayCharge = 1000;
+        if (age < 15) {
+            baseSubwayCharge = baseSubwayCharge * 0.5;
+        }else if(age > 60) {
+            baseSubwayCharge = baseSubwayCharge * 0.7;
+        }
+        if(kilometer > 50) {
+            return baseSubwayCharge * 1.5;
+        }else {
+            return baseSubwayCharge;
+        }
+    }
+}
+
+
+
